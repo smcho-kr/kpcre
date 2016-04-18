a09d311e5cd6dc71978d855d049feeecd71bc6f8
Seongmyun Cho <highsky@gmail.com>
2016-04-18 15:23:35 +0900

    change license

--- libc/libc.c
+++ libc/libc.c
@@ -1,6 +1,22 @@
-/*
- *  * main.c − The kernel libc module.
- *   */
+/* 
+ * libc.c	Kernel C library wrapper
+ *
+ * Copyright (C) 2016 Seongmyun Cho <highsky@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #include <linux/types.h>
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
